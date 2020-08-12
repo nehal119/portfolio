aa30c7c8a638a99c5d7a3a1473dda9fdb8b71302
Nehal Ahmad <nehal.ahmad@arnowa.com>
2020-08-12 21:28:46 +0530

    minor changes

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -3,14 +3,14 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import BlogsImg from "./BlogsImg";
+// import BlogsImg from "./BlogsImg";
 import AddressImg from "./AddressImg";
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-const blogSection = contactPageData.blogSection;
+// const blogSection = contactPageData.blogSection;
 const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
 
